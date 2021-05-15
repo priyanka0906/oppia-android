a7908fba34a2f5c4fb6224aaa83dcb21c1791ff9
priyanka0906 <priyanka02amb@reddiffmail.com>
2021-05-15 13:49:07 +0530

    changed to PromotedCarouselListViewModel.kt

--- app/src/main/java/org/oppia/android/app/shim/ViewBindingShimImpl.kt
+++ app/src/main/java/org/oppia/android/app/shim/ViewBindingShimImpl.kt
@@ -19,7 +19,6 @@ import org.oppia.android.databinding.DragDropSingleItemBinding
 import org.oppia.android.databinding.ItemSelectionInteractionItemsBinding
 import org.oppia.android.databinding.MultipleChoiceInteractionItemsBinding
 import org.oppia.android.databinding.PromotedCarouselItemCardBinding
-import org.oppia.android.databinding.PromotedStoryCardBinding
 import org.oppia.android.util.parser.html.HtmlParser
 import javax.inject.Inject
 
